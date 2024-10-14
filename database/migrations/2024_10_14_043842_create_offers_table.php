fd1c5a565384f18b19fc1e98a6cbaeb808fa604a
mattu23 <hirokiii_27@yahoo.co.jp>
2024-10-14 14:34:48 +0900

    migrationファイルの作成/usersテーブルの定義

---
+++ database/migrations/2024_10_14_043842_create_offers_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+//   id
+// - name
+// - types（=status）
+// - category
+// - prefecture
+// - file
+// - detail
+// - qualification
+// - announcement_date
+// - bid_date
+// - winning_bid
+// - bookmark
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offers');
+    }
+};
---
+++ database/migrations/2024_10_14_043853_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password', 255);
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('user_roles');
+            $table->unsignedBigInteger('company_id');
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->timestamps();
+            $table->softDeletes('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
