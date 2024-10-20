10b88a518389a120371cf03fc3add3e3b47aeb8b
mattu23 <hirokiii_27@yahoo.co.jp>
2024-10-21 00:11:23 +0900

    各種ドメイン層の実装

---
+++ app/Packages/Domains/Organization/SearchOrganization.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Packages\Domains\Organization;
+
+
+class SearchOrganization
+{
+  /**
+   * @var int
+   */
+  private int $id;
+
+  /**
+   * @var string
+   */
+  private string $name;
+  
+
+  public function __construct(int $id, string $name)
+  {
+    $this->id = $id;
+    $this->name = $name;
+  }
+
+  public function getId(): int
+  {
+    return $this->id;
+  }
+
+  public function getName(): string
+  {
+    return $this->name;
+  }
+}
+
---
+++ app/Packages/Domains/Qualification/Qualification.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Packages\Domains\Qualification;
+
+class SearchQualification
+{
+  /**
+   * @var int
+   */
+  private int $id;
+
+  /**
+   * @var string
+   */
+  private string $name;
+  
+
+  public function __construct(int $id, string $name)
+  {
+    $this->id = $id;
+    $this->name = $name;
+  }
+
+  public function getId(): int
+  {
+    return $this->id;
+  }
+
+  public function getName(): string
+  {
+    return $this->name;
+  }
+}
+
